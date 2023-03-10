5b3b6ff8556ec82781deb877feb23beff4992f92
ssss_Minami <2401406158@qq.com>
2023-03-10 16:03:46 +0800

    kalman update

--- Core/Inc/kalman.h
+++ Core/Inc/kalman.h
@@ -11,7 +11,23 @@ typedef struct
 	float output;
 	float output_Max;
 }Kalman_TypeDef;
+typedef struct
+{
+	float R;
+	float Q_angle;
+	float Q_gyro;
+
+	float angle;
+	float Q_bias;
+
+	float P[2][2];
+	float K[2];
+	float output;
+}IMU_fliter_TypeDef;
 extern Kalman_TypeDef Klm_Motor[6];
+extern IMU_fliter_TypeDef imu_fliter[3];
 extern void KalmanFilter_Init(Kalman_TypeDef *klm_typedef);
 extern float KalmanFilter(Kalman_TypeDef *f, float input);
+extern void IMU_fliter_Init(IMU_fliter_TypeDef *f);
+extern void IMU_fliter(IMU_fliter_TypeDef *f,float newangle,float newgyro);
 #endif /* INC_KALMAN_H_ */
--- Core/Src/kalman.c
+++ Core/Src/kalman.c
@@ -1,4 +1,5 @@
 #include "kalman.h"
+#include "math.h"
 void KalmanFilter_Init(Kalman_TypeDef *klm_typedef)
 {
 	klm_typedef->K = 0;
@@ -18,12 +19,46 @@ float KalmanFilter(Kalman_TypeDef *f, float input)
 	f->p_now = f->p_past + f->Q;
 	f->K = f->p_now / (f->p_now + f->R);
 	f->output = f->output + f->K * (input - f->output);
-//	if(f->output > f->output_Max)
-//	{
-//		f->output = f->output_Max;
-//	}
 
 	f->p_past = (1 - f->K) * f->p_now;
 	return f->output;
 }
 Kalman_TypeDef Klm_Motor[6];
+IMU_fliter_TypeDef imu_fliter[3];
+void IMU_fliter(IMU_fliter_TypeDef *f,float newangle,float newgyro)
+{
+	const float dt = 0.01;
+	f->angle = f->angle - (f->Q_bias-newgyro)*dt;
+
+	f->P[0][0] = f->P[0][0] + f->Q_angle - (f->P[0][1]-f->P[1][0])*dt + (f->P[1][1]*dt*dt);
+	f->P[0][1] = f->P[0][1] - (f->P[1][1]*dt);
+	f->P[1][0] = f->P[1][0] - (f->P[1][1]*dt);
+	f->P[1][1] = f->P[1][0] + f->Q_gyro;
+
+	f->K[0] = f->P[0][0]/(f->P[0][0] + f->R);
+	f->K[1] = f->P[1][0]/(f->P[0][0] + f->R);
+	if(f->K[0] == 1)
+		f->K[0] = 0.5;
+
+	f->angle = f->angle + f->K[0]*(newangle - f->angle);
+	f->Q_bias = f->Q_bias + f->K[1]*(newangle - f->angle);
+	f->output = f->angle;
+
+	f->P[0][0] = f->P[0][0] - (f->K[0]*f->P[0][0]);
+	f->P[0][1] = f->P[0][1] - (f->K[0]*f->P[0][1]);
+	f->P[1][0] = f->P[1][0] - (f->K[1]*f->P[1][0]);
+	f->P[1][1] = f->P[1][1] - (f->K[1]*f->P[1][1]);
+}
+
+void IMU_fliter_Init(IMU_fliter_TypeDef *f)
+{
+	f->P[0][0] = 1;
+	f->P[0][1] = 1;
+	f->P[1][0] = 1;
+	f->P[1][1] = 1;
+	f->K[0] = 0;
+	f->K[1] = 0;
+	f->Q_angle = 0.001;
+	f->Q_gyro = 0.003;
+	f->R = 0.03;
+}
